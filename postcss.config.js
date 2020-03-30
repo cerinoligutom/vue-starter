ad28ef60f794635d920dc12bde8905b3bf3959a9
Cerino O. Ligutom III <zeferinix@gmail.com>
2020-03-31 01:19:12 +0800

    Added postcss-nested

--- postcss.config.js
+++ postcss.config.js
@@ -15,6 +15,7 @@ module.exports = {
 	plugins: [
 		require('postcss-import'),
 		require('tailwindcss'),
+		require('postcss-nested'),
 		require('autoprefixer'),
 		...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
 	],
